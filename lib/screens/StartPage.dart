f3f36f1c39ddc62032cd05867bc0b106b6291879
Aayushi Gupta <47745321+aayushigupt@users.noreply.github.com>
2020-04-01 18:52:19 +0530

    Update StartPage.dart
    Made some changes in animation

--- lib/screens/StartPage.dart
+++ lib/screens/StartPage.dart
@@ -1,62 +1,54 @@
-import 'package:flutter/material.dart' show AnimatedBuilder, Animation, AnimationController, BuildContext, Center, Colors, Container, CurvedAnimation, Curves, FloatingActionButton, FontWeight, Icon, Icons, Matrix4, MediaQuery, Scaffold, SingleTickerProviderStateMixin, State, StatefulWidget, Text, TextStyle, Transform, Tween, Widget;
+import 'package:flutter/material.dart';
 import 'package:flutter/animation.dart';
+import 'loginPage.dart';
 
 
-class Home extends StatefulWidget
-{
+class Home extends StatefulWidget{
   @override
-   createState() {
-    // TODO: implement createState
+  createState() {
     return HomeState();
   }
 }
-class HomeState extends State<Home> with SingleTickerProviderStateMixin
-{
+
+class HomeState extends State<Home> with SingleTickerProviderStateMixin{
   Animation animation;
   AnimationController animationController;
   
 
   @override
-  void initState()
-  {
-    
+  void initState(){
     super.initState();
-    animationController = 
-            AnimationController(duration: Duration(seconds: 2), vsync:this );
+    animationController = AnimationController(duration: Duration(seconds: 2), vsync:this );
     animation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(parent: animationController, curve: Curves.fastOutSlowIn));
     animationController.forward();
-  
+    animationController.addStatusListener((listener){
+      if(listener == AnimationStatus.completed){
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (BuildContext context) => Login()
+          )
+        );
+      }
+    });
   }
+
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context){
     final double width = MediaQuery.of(context).size.width;
     return AnimatedBuilder(
       animation: animationController,
       builder: (BuildContext context, Widget child){
         return Scaffold(
-          body: Transform(transform: Matrix4.translationValues(animation.value*width, 0.0, 0.0),
-          child: new Center(
-            child: Container(child: Text('Scan - Q',style: TextStyle(fontSize:20.0, fontWeight: FontWeight.bold ),),),
-          ),
-                
-          
-          ),
-          
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.arrow_right, size:40.0),
-          backgroundColor: Colors.blue,
-          
-        ),
-        
-        
-        
+          body: Transform(
+            transform: Matrix4.translationValues(animation.value*width, 0.0, 0.0),
+            child: new Center(
+              child: Container(child: Text('Scan - Q',style: TextStyle(fontSize:20.0, fontWeight: FontWeight.bold ),),),
+            ),
+          ),  
+
         );
       }
     );
   }
-
-
-
 }
